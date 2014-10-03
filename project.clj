b5cb5e48d081c0271bc95bf5a9855681b9a631a2
Kyle Oba <koba@mudphone.com>
2014-10-03 09:28:54 -1000

    Add basic blank quil project with localrepo OpenCV jars.

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject videotest "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [quil "2.2.2"]
+                 [opencv/opencv "2.4.10"]
+                 [opencv/opencv-native "2.4.10"]]
+  :profiles {:dev {:source-paths ["dev"]}})
