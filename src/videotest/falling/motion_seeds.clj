c984a6db0aaad9868851eb4c2ce441af9f3825e8
Kyle Oba <koba@mudphone.com>
2014-10-27 02:29:02 -1000

    Add color of underlying grid to motion seeds.

--- src/videotest/falling/motion_seeds.clj
+++ src/videotest/falling/motion_seeds.clj
@@ -6,23 +6,23 @@
 
 (def SEED-W 20)
 (def SEED-CREATE-ODDS 0.1)
-(def SEED-RGBA [255 0 255 100])
 
 (defn seed-created? []
   (> SEED-CREATE-ODDS (rand)))
 
-(defn init-motion-seed [x y]
-  {:x x :y y})
+(defn init-motion-seed [x y color]
+  {:x x :y y :color color})
 
-(defn create-motion-seeds [bin-size {:keys [motion-trace motion-seeds] :as state}]
+(defn create-motion-seeds [bin-size {:keys [motion-trace motion-seeds color-record] :as state}]
   (assoc-in state [:motion-seeds]
-            (reduce (fn [memo [[col row] {:keys [life]}]]
+            (reduce (fn [memo [[col row :as coords] {:keys [life]}]]
                       (if (and (<= mtrace/MAX-TRACE-LIFE life)
                                (seed-created?))
                         (conj memo (init-motion-seed (+ (* col bin-size)
                                                         (rand bin-size))
                                                      (+ (* row bin-size)
-                                                        (rand bin-size))))
+                                                        (rand bin-size))
+                                                     (color-record coords)))
                         memo))
                     motion-seeds
                     motion-trace)))
@@ -45,15 +45,15 @@
        (create-motion-seeds bin-size)
        (move-motion-seeds display-height)))
 
-(defn draw-motion-seed [x y]
+(defn draw-motion-seed [x y color]
+  (apply q/fill color)
   (q/ellipse x y SEED-W SEED-W))
 
 (defn draw-motion-seeds [{:keys [motion-seeds]}]
   (q/push-style)
   (q/no-stroke)
-  (apply q/fill SEED-RGBA)
   (dorun
-   (map (fn [{:keys [x y]}]
-          (draw-motion-seed x y))
+   (map (fn [{:keys [x y color]}]
+          (draw-motion-seed x y color))
         motion-seeds))
   (q/pop-style))
